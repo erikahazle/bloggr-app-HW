5e9ab4cf34c8df848399b527299d83b3fd6ede80
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-06-18 15:28:04 +0100

    Basic express app setup

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', './public/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+app.listen(port, function() {
+  console.log('tj is cool!');
+});
